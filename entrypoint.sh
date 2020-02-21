2b71e00b6590c08b3985224b1cf4c29588561593
Day <day@playstudios.asia>
2020-02-21 14:30:27 +0800

    Remove --grpc-web

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ curl -sSL -o /usr/local/bin/argocd https://${ARGOCD_SERVER}/download/argocd-linu
 chmod +x /usr/local/bin/argocd
 
 echo "==> Syncing app..."
-argocd app sync $1 --grpc-web
+argocd app sync $1
 
 echo "==> Waiting for app to become healthy..."
-argocd app wait $1 --grpc-web
+argocd app wait $1
 
 echo "==> Generating app env url output..."
-env_url=$(argocd app get $1 --grpc-web -o json | jq -r '.status.summary.externalURLs | sort_by(length) | .[0]')
+env_url=$(argocd app get $1 -o json | jq -r '.status.summary.externalURLs | sort_by(length) | .[0]')
 echo "::set-output name=env_url::${env_url}"
